c67509c858d4026e1eb55594fbcbeb968ac7bea6
Mahesh Maida <mahesh.maida.7489@gmail.com>
2025-09-01 23:14:34 +0530

    Updated

---
+++ project-02-class-report-generator/index.js
@@ -0,0 +1,74 @@
+let array = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    marks: { Math: 88, Science: 92, English: 81 },
+    attendance: 95,
+  },
+  {
+    id: 2,
+    name: "Priya Das",
+    marks: { Math: 76, Science: 85, English: 78 },
+    attendance: 88,
+  },
+  {
+    id: 3,
+    name: "Rohan Mehta",
+    marks: { Math: 65, Science: 72, English: 69 },
+    attendance: 80,
+  },
+  {
+    id: 4,
+    name: "Sneha Patel",
+    marks: { Math: 92, Science: 89, English: 94 },
+    attendance: 98,
+  },
+  {
+    id: 5,
+    name: "Kabir Khan",
+    marks: { Math: 55, Science: 60, English: 58 },
+    attendance: 70,
+  },
+];
+
+function objtostr(stdDetail) {
+  let str = 0;
+  for (subject in stdDetail.marks) {
+    str += stdDetail.marks[subject];
+  }
+  return str;
+}
+
+function getPercentage(total) {
+  let percentage = 0;
+  percentage = total / 3;
+  return percentage;
+}
+
+function getGrade(percentage) {
+  if (percentage >= 90 && percentage <= 100) {
+    return "A";
+  } else if (percentage >= 75 && percentage <= 90) {
+    return "B";
+  } else if (percentage >= 55 && percentage <= 75) {
+    return "C";
+  } else if (percentage >= 30 && percentage <= 55) {
+    return "D";
+  } else {
+  }
+}
+
+let total = 0;
+let percentage = 0;
+let grade = null;
+array.forEach(function (stdDetail) {
+  id = stdDetail.id;
+  name = stdDetail.name;
+  total = objtostr(stdDetail);
+  percentage = getPercentage(total);
+  grade = getGrade(percentage);
+  info = `${id},${name},${total},${percentage},${grade}`;
+  console.log(info);
+});
+
+console.log(info);
