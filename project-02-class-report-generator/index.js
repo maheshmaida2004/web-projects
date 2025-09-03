5fe4ded05a68e02817dc9832e9092e7e6303bab4
Mahesh Maida <mahesh.maida.7489@gmail.com>
2025-09-03 10:19:33 +0530

    done

--- project-02-class-report-generator/index.js
+++ project-02-class-report-generator/index.js
@@ -1,49 +1,7 @@
-let array = [
-  {
-    id: 1,
-    name: "Aarav Sharma",
-    marks: { Math: 88, Science: 92, English: 81 },
-    attendance: 95,
-  },
-  {
-    id: 2,
-    name: "Priya Das",
-    marks: { Math: 76, Science: 85, English: 78 },
-    attendance: 88,
-  },
-  {
-    id: 3,
-    name: "Rohan Mehta",
-    marks: { Math: 65, Science: 72, English: 69 },
-    attendance: 80,
-  },
-  {
-    id: 4,
-    name: "Sneha Patel",
-    marks: { Math: 92, Science: 89, English: 94 },
-    attendance: 98,
-  },
-  {
-    id: 5,
-    name: "Kabir Khan",
-    marks: { Math: 55, Science: 60, English: 58 },
-    attendance: 70,
-  },
-];
+const fs = require("fs");
 
-function objtostr(stdDetail) {
-  let str = 0;
-  for (subject in stdDetail.marks) {
-    str += stdDetail.marks[subject];
-  }
-  return str;
-}
-
-function getPercentage(total) {
-  let percentage = 0;
-  percentage = total / 3;
-  return percentage;
-}
+const data = fs.readFileSync("./data.json", "utf8");
+const studentsData = JSON.parse(data);
 
 function getGrade(percentage) {
   if (percentage >= 90 && percentage <= 100) {
@@ -57,18 +15,49 @@ function getGrade(percentage) {
   } else {
   }
 }
+let studentInformation = "";
+let totalPercentage = 0;
+let averagePercentage = 0;
+let highPercentage = 0;
+let topper = null;
+let below70Students = [];
+let greater75Attendence = [];
+studentsData.forEach(function (studentsInfo) {
+  const { id, name, marks } = studentsInfo;
+  const total = Object.values(marks).reduce(function (acc, ele) {
+    return acc + ele;
+  }, 0);
 
-let total = 0;
-let percentage = 0;
-let grade = null;
-array.forEach(function (stdDetail) {
-  id = stdDetail.id;
-  name = stdDetail.name;
-  total = objtostr(stdDetail);
-  percentage = getPercentage(total);
+  const grandTotal = Object.values(marks).length * 100;
+  const percentage = (total / grandTotal) * 100;
   grade = getGrade(percentage);
-  info = `${id},${name},${total},${percentage},${grade}`;
-  console.log(info);
+
+  totalPercentage += percentage;
+  averagePercentage = totalPercentage / studentsData.length;
+
+  if (highPercentage < percentage) {
+    highPercentage = percentage;
+    topper = name;
+  }
+
+  if (percentage < 70) {
+    below70Students.push(name);
+  }
+  if (percentage < 75) {
+    greater75Attendence.push(name);
+  }
+
+  studentInformation += `${id}. ${name} - Total: ${total} | Percentage: ${percentage.toFixed(
+    2
+  )}% | Grade: ${grade}\n`;
 });
 
-console.log(info);
+console.log(studentInformation);
+console.log(`
+  --------------------------------------------
+Class Summary:
+- Class Average Percentage: ${averagePercentage}%
+- Top Performer: ${topper}
+- Students Below 70%: ${below70Students}
+- Students with Low Attendance (<75%): ${greater75Attendence}
+--------------------------------------------`);
