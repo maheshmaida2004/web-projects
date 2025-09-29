c01f9667c9eb9c0ce2eaffb45d72d655bcef7e31
Mahesh Maida <mahesh20040501@gmail.com>
2025-09-29 23:14:44 +0530

    completed formating movie recomadation project

--- practice-task-06/index.js
+++ practice-task-06/index.js
@@ -1,70 +1,60 @@
-const { count } = require("console");
 const fs = require("fs");
-const data = fs.readFileSync("./data.json", "utf-8");
-const custmerData = JSON.parse(data);
-function appendtext(data) {
-  fs.appendFileSync("output.txt", data, "utf-8");
-}
+const jsonData = fs.readFileSync("./data.json", "utf-8");
+const objData = JSON.parse(jsonData);
+
+const appendText = function (text) {
+  fs.appendFileSync("output.txt", text, "utf-8");
+};
+
 let totalRevenue = 0;
-let totalOrders = 0;
-let uniqueCustomer = {};
-let custmerWiseRevenue = {};
-let catagortWiseRevenue = {};
-let orderByStatus = {};
-custmerData.forEach(function (products) {
-  const { customer, items, status } = products;
-  items.forEach(function (item) {
-    const { category, price, quantity } = item;
-    const revenue = price * quantity;
+let totalOrders = objData.length;
+let orderCountByStatus = {};
+let revenueByCustomers = {};
+let revenueByCategory = {};
 
-    totalRevenue += revenue;
-    totalOrders = custmerData.length;
-
-    if (uniqueCustomer[customer] === undefined) {
-      uniqueCustomer[customer] = { name: customer, count: 0 };
-    }
-    uniqueCustomer[customer].count++;
-
-    if (custmerWiseRevenue[customer] === undefined) {
-      custmerWiseRevenue[customer] = { name: customer, salary: 0 };
-    }
-    custmerWiseRevenue[customer].salary += revenue;
-    if (catagortWiseRevenue[category] === undefined) {
-      catagortWiseRevenue[category] = { name: category, salary: 0 };
-    }
-    catagortWiseRevenue[category].salary += revenue;
-  });
-  if (orderByStatus[status] === undefined) {
-    orderByStatus[status] = { name: status, count: 0 };
+const appendObj = function (obj, formatStr) {
+  for (let key in obj) {
+    let str = formatStr;
+    str = str.replaceAll("#key", key);
+    str = str.replaceAll("#value", obj[key]);
+    appendText(str);
   }
-  orderByStatus[status].count++;
+};
+
+const addValueToObj = function (obj, key, value) {
+  if (obj[key] === undefined) obj[key] = 0;
+  obj[key] += value;
+};
+
+objData.forEach(function (order) {
+  const { customer, items, status } = order;
+  addValueToObj(orderCountByStatus, status, 1);
+  let orderRevenue = 0;
+  items.forEach(function (product) {
+    const { product: name, category, price, quantity } = product;
+    const itemRevnue = price * quantity;
+    totalRevenue += itemRevnue;
+    orderRevenue += itemRevnue;
+    addValueToObj(revenueByCategory, category, itemRevnue);
+  });
+  addValueToObj(revenueByCustomers, customer, orderRevenue+);
 });
 
-let unique = Object.keys(uniqueCustomer).length;
-appendtext(`
-    "Total Revenue: ${totalRevenue}
-    "Unique Customers: ${unique}
-    "Total Orders:${totalOrders}\n
-    `);
+appendText(`
+-----------------------------------------
+         E-COMMERCE ORDER REPORT
+-----------------------------------------
+`);
 
-function revenueastrToObj(custmerWiseRevenue) {
-  for (let key in custmerWiseRevenue) {
-    appendtext(`${key}:${custmerWiseRevenue[key].salary}\n`);
-  }
-}
+appendText(`
+Total Revenue: ₹${totalRevenue}
+Total Orders: ${totalOrders}
+Unique Customers: ${Object.keys(revenueByCustomers).length}
+`);
 
-function revenueCustmerWise(catagortWiseRevenue) {
-  for (let key in catagortWiseRevenue) {
-    appendtext(`${key}= ${catagortWiseRevenue[key].salary}\n`);
-  }
-}
-
-function orderWise(orderByStatus) {
-  for (let key in orderByStatus) {
-    appendtext(` ${key} = ${orderByStatus[key].count}\n`);
-  }
-}
-
-revenueastrToObj(custmerWiseRevenue);
-revenueCustmerWise(catagortWiseRevenue);
-orderWise(orderByStatus);
+appendText(`\nRevenue by Customer:\n`);
+appendObj(revenueByCustomers, `- #key: ₹#value\n`);
+appendText(`\nRevenue by Category:\n`);
+appendObj(revenueByCategory, `- #key: ₹#value\n`);
+appendText(`\nOrders by Status:\n`);
+appendObj(orderCountByStatus, `- #key: #value\n`);
